80fe0176b86e95770e5f9944ad616259749396bd
Jeff Wang <cjw39@hotmail.com>
2014-02-17 15:00:02 -0600

    Add singleton.

--- Timer.java
+++ Timer.java
@@ -3,18 +3,34 @@
  * Date  : Feb 15, 2014
  *
  * Time class is used to measure elapsed time. This class uses System.nanoTime().
+ *
+ * The singleton instance of this class is not thread-safe.
  */
 public class Timer
 {
     public long startTime;
     public long endTime;
- 
+    
+    private static Timer singleton;
+    
     /**
      * Constructs an instance and set start time.
      */   
     public Timer()
     {
         startTime = System.nanoTime();
+    }
+    
+    /**
+     * Returns the singleton instance.
+     * This method returns the Timer object if already exsit, else a new instace.
+     * If the object already exists, calling this method does not reset the startTime of the singleton instance.
+     */
+    public static Timer getTimer()
+    {
+        if(singleton == null)
+            singleton = new Timer();
+        return singleton;
     }
     
     /**
