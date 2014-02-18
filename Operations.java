b65cd0c90ee55b6bc826c654f180dbab7a6f3b57
Jeff Wang <cjw39@hotmail.com>
2014-02-17 22:31:50 -0600

    Add GetRootNS().

--- Operations.java
+++ Operations.java
@@ -34,4 +34,28 @@ public class Operations {
 
         return id;
     }
+
+    /**
+     * Root operation via nested sets model.
+     * 
+     * @param tableName name of the table to run query.
+     * @param nodeId the ID of the starting node. This node must be in the table specified by tableName.
+     * @return ID of the root node; -1 if failure.
+     */
+    public static int GetRootNS(String tableName, int nodeId) throws IOException, SQLException
+    {
+        Connection con = Database.connect();
+        Statement s = con.createStatement();
+
+        //Check nodeId is valid
+        ResultSet rset = s.executeQuery("select * from " + tableName + " where id=" + nodeId);
+        if(!rset.next()) return -1;
+
+        int l = rset.getInt("l");
+        int r = rset.getInt("r");
+        String query = String.format("select * from %1s where l<= %2d and r >= %3d and parent is NULL", tableName, l, r);
+        rset = s.executeQuery(query);
+        rset.next();
+        return rset.getInt("id");
+    }
 }
