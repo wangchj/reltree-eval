ad68f29e2148c49b591ff92356f91cbd73f300ca
Jeff Wang <cjw39@hotmail.com>
2014-02-18 22:03:56 -0600

    Add siblings(String tableName, int nodeId).

--- Operations.java
+++ Operations.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  * Implementation of the operations defined in the publication.
@@ -13,7 +14,7 @@ public class Operations {
      * @param nodeId the ID of the starting node. This node must be in the table specified by tableName.
      * @return ID of the root node; -1 if failure.
      */
-    public static int GetRootAL(String tableName, int nodeId) throws IOException, SQLException
+    public static int rootAL(String tableName, int nodeId) throws IOException, SQLException
     {
         Connection con = Database.connect();
         Statement s = con.createStatement();
@@ -42,7 +43,7 @@ public class Operations {
      * @param nodeId the ID of the starting node. This node must be in the table specified by tableName.
      * @return ID of the root node; -1 if failure.
      */
-    public static int GetRootNS(String tableName, int nodeId) throws IOException, SQLException
+    public static int rootNS(String tableName, int nodeId) throws IOException, SQLException
     {
         Connection con = Database.connect();
         Statement s = con.createStatement();
@@ -57,5 +58,19 @@ public class Operations {
         rset = s.executeQuery(query);
         rset.next();
         return rset.getInt("id");
+    }
+    
+    public static ArrayList<Integer> siblings(String tableName, int nodeId) throws IOException, SQLException
+    {
+        Connection con = Database.connect();
+        Statement s = con.createStatement();
+        String query = String.format("select * from %1$s where parent=(select parent from %1$s where id = %2$d) and id != %2$d", tableName, nodeId);
+        ResultSet rs = s.executeQuery(query);
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        while(rs.next())
+        {
+            result.add(rs.getInt("id"));
+        }
+        return result;
     }
 }
