253e77561de28c93d7283b1f9fe70682499683c0
Jeff Wang <cjw39@hotmail.com>
2014-02-23 23:02:41 -0600

    Enumeration for supported operations and implementing algorithms.

---
+++ Operation.java
@@ -0,0 +1,21 @@
+/**
+ * Author: Chih-Jye Wang
+ * Date  : Feb 21, 2014
+ */
+ 
+ /**
+ * Operation represents a set of tree operations that are listed in the publication.
+ */
+enum Operation
+{
+    Root, Parent, Children, Siblings, Leaves, Height, Depth, Path, Member, Tree
+}
+
+/**
+ * A list of relational data models (algorithms) in which the operations are implemented.
+ */ 
+enum Algo
+{
+    AL, //Adjacency list
+    NS, //Nested Setspara
+}
